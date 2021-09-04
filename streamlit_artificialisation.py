776cb671c700f3235775194456ecb1a666b56642
PomPotesbio <54704006+PomPotesbio@users.noreply.github.com>
2021-09-04 11:39:54 +0200

    Update streamlit_artificialisation.py

--- streamlit_artificialisation.py
+++ streamlit_artificialisation.py
@@ -20,7 +20,7 @@ file_bytes = st.file_uploader("Upload a file", type=("png", "jpg"))
 
 if file_bytes == None:
     st.warning('No file selected.')
-  else:
+else:
     image2 = Image.open(file_bytes)
     st.image(image2, width=500)
 

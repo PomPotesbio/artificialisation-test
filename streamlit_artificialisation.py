a2e4b7b3c54b026ee8550fa5cdb05243a760587b
PomPotesbio <54704006+PomPotesbio@users.noreply.github.com>
2021-09-08 11:39:57 +0200

    Update streamlit_artificialisation.py

--- streamlit_artificialisation.py
+++ streamlit_artificialisation.py
@@ -17,7 +17,7 @@ st.write('''"Ce phénomène consiste à transformer un sol naturel, agricole ou 
 [Ministère de la transition écologique](https://www.ecologie.gouv.fr/artificialisation-des-sols) ''')
 
 ## User input
-file_bytes = st.file_uploader("Upload a tile", type=("png", "jpg")) 
+file_bytes = st.file_uploader("Upload a tile", type=("png", "jpg", "tif", "tiff", "jp2")) 
 
 if file_bytes == None:
     st.warning('No file selected. Please select a file.')

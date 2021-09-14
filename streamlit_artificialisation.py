ed255a8b65de8faf66f587962c4218d8434f4c67
PomPotesbio <54704006+PomPotesbio@users.noreply.github.com>
2021-09-14 09:53:29 +0200

    Update streamlit_artificialisation.py

--- streamlit_artificialisation.py
+++ streamlit_artificialisation.py
@@ -71,6 +71,7 @@ else:
     img_arr=array_masks, # required - array of cropped out images
     org_img_size=im_size)
     
-    final_pred=Image.fromarray(x_reconstructed[0]).resize((600,600))
+    final_pred=Image.fromarray(x_reconstructed[0])
+    final_pred.resize((600,600))
     st.image(final_pred, caption="L'artificialisation de la ville choisie")
     
